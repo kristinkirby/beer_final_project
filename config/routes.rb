f8b819c7d57e621da57e7662f3d4f883810a1c03
kristinkirby <kristin.kirby@chicagobooth.edu>
2021-02-25 21:24:17 +0000

    played around with diff styles on beer page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,94 @@
 Rails.application.routes.draw do
   
+  
+  # Routes for the Rating resource:
+
+  # CREATE
+  post("/insert_rating", { :controller => "ratings", :action => "create" })
+          
+  # READ
+  get("/ratings", { :controller => "ratings", :action => "index" })
+  get("/ratings/:path_id", { :controller => "ratings", :action => "show" })
+  
+  # UPDATE
+  post("/modify_rating/:path_id", { :controller => "ratings", :action => "update" })
+  
+  # DELETE
+  get("/delete_rating/:path_id", { :controller => "ratings", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Brewery resource:
+
+  # CREATE
+  post("/insert_brewery", { :controller => "breweries", :action => "create" })
+          
+  # READ
+  get("/breweries", { :controller => "breweries", :action => "index" })
+  get("/breweries/:path_id", { :controller => "breweries", :action => "show" })
+  
+  # UPDATE
+  post("/modify_brewery/:path_id", { :controller => "breweries", :action => "update" })
+  
+  # DELETE
+  get("/delete_brewery/:path_id", { :controller => "breweries", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Beer resource:
+
+  # CREATE
+  post("/insert_beer", { :controller => "beers", :action => "create" })
+          
+  # READ
+  get("/beers", { :controller => "beers", :action => "index" })
+  get("/beers/:path_id", { :controller => "beers", :action => "show" })
+  
+  # UPDATE
+  post("/modify_beer/:path_id", { :controller => "beers", :action => "update" })
+  
+  # DELETE
+  get("/delete_beer/:path_id", { :controller => "beers", :action => "destroy" })
+
+  #------------------------------
+
+  # Homepage
+  get("/", {:controller => "application", :action => "testpage"})  
+
+  # Routes for the User account:
+  # SIGN UP FORM
+  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })        
+  # CREATE RECORD
+  post("/insert_user", { :controller => "user_authentication", :action => "create"  })
+      
+  # EDIT PROFILE FORM        
+  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
+  # UPDATE RECORD
+  post("/modify_user", { :controller => "user_authentication", :action => "update" })
+  # DELETE RECORD
+  get("/cancel_user_account", { :controller => "user_authentication", :action => "destroy" })
+
+  # ------------------------------
+
+  # SIGN IN FORM
+  get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
+  # AUTHENTICATE AND STORE COOKIE
+  post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
+  # SIGN OUT        
+  get("/user_sign_out", { :controller => "user_authentication", :action => "destroy_cookies" })
+  #------------------------------
+  
+
+
+
+
+
+
+
+
+
+
+
   # Routes for the Flag resource:
   # CREATE
   post("/insert_flag", { :controller => "flags", :action => "create" })
@@ -54,88 +143,5 @@ Rails.application.routes.draw do
 
   #------------------------------
 
-  # Routes for the Rating resource:
-
-  # CREATE
-  post("/insert_rating", { :controller => "ratings", :action => "create" })
-          
-  # READ
-  get("/ratings", { :controller => "ratings", :action => "index" })
-  
-  get("/ratings/:path_id", { :controller => "ratings", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_rating/:path_id", { :controller => "ratings", :action => "update" })
-  
-  # DELETE
-  get("/delete_rating/:path_id", { :controller => "ratings", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Brewery resource:
-
-  # CREATE
-  post("/insert_brewery", { :controller => "breweries", :action => "create" })
-          
-  # READ
-  get("/breweries", { :controller => "breweries", :action => "index" })
-  
-  get("/breweries/:path_id", { :controller => "breweries", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_brewery/:path_id", { :controller => "breweries", :action => "update" })
-  
-  # DELETE
-  get("/delete_brewery/:path_id", { :controller => "breweries", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Beer resource:
-
-  # CREATE
-  post("/insert_beer", { :controller => "beers", :action => "create" })
-          
-  # READ
-  get("/beers", { :controller => "beers", :action => "index" })
-  
-  get("/beers/:path_id", { :controller => "beers", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_beer/:path_id", { :controller => "beers", :action => "update" })
-  
-  # DELETE
-  get("/delete_beer/:path_id", { :controller => "beers", :action => "destroy" })
-
-  #------------------------------
-
-  # Homepage
-  get("/", {:controller => "application", :action => "testpage"})  
-
-  # Routes for the User account:
-  # SIGN UP FORM
-  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })        
-  # CREATE RECORD
-  post("/insert_user", { :controller => "user_authentication", :action => "create"  })
-      
-  # EDIT PROFILE FORM        
-  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
-  # UPDATE RECORD
-  post("/modify_user", { :controller => "user_authentication", :action => "update" })
-  # DELETE RECORD
-  get("/cancel_user_account", { :controller => "user_authentication", :action => "destroy" })
-
-  # ------------------------------
-
-  # SIGN IN FORM
-  get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
-  # AUTHENTICATE AND STORE COOKIE
-  post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
-  # SIGN OUT        
-  get("/user_sign_out", { :controller => "user_authentication", :action => "destroy_cookies" })
-  #------------------------------
-  
 
 end
